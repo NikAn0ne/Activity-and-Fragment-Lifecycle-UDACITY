13858b576469234aa58430a17e0ba510b32e0fb7
NikAn0ne <nikan0ne@mail.ru>
2022-09-18 18:04:55 +0300

    Add onSavedInstance

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -18,7 +18,7 @@ package com.example.android.dessertpusher
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
-import android.util.Log
+import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -28,6 +28,11 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleObserver
 import com.example.android.dessertpusher.databinding.ActivityMainBinding
 import timber.log.Timber
+
+private const val KEY_REVENUE = "key_revenue"
+private const val KEY_DESSERTS_SOLD = "deserts_sold"
+private const val KEY_DESSERT_TIMER = "desert_timer"
+private const val KEY_DESSERT_IMMAGE = "dessert_immage"
 
 class MainActivity : AppCompatActivity(), LifecycleObserver {
 
@@ -76,6 +81,13 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
         binding.dessertButton.setOnClickListener {
             onDessertClicked()
+        }
+
+        if (savedInstanceState != null)
+        {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERTS_SOLD, 0)
+            dessertTimer.secondsCount = savedInstanceState.getInt(KEY_DESSERT_TIMER, 0)
         }
 
         // Set the TextViews to the right values
@@ -152,6 +164,15 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             R.id.shareMenuButton -> onShare()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERTS_SOLD, dessertsSold)
+        outState.putInt(KEY_DESSERT_TIMER, dessertTimer.secondsCount)
+        outState.putInt(KEY_DESSERT_IMMAGE, currentDessert.imageId)
     }
 
     override fun onStart() {
